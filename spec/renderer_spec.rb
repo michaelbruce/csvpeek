99c3f4a4cec0f4aaa17b05c18df14529ea1c6c6c
Michael Bruce <michael.bruce@singletracksystems.com>
2015-09-12 16:17:16 +0100

    messy, cells/headers now align

--- spec/renderer_spec.rb
+++ spec/renderer_spec.rb
@@ -1,7 +1,18 @@
 require_relative 'spec_helper'
 
 describe SheetRender do
-  it 'exists' do
-    puts 'hii'
+
+  let(:sheet) { [['Id','Name'], ['1','Alex'], ['2','Hannah']] }
+
+  it 'returns the longest value for a given column' do
+    expect(SheetRender.new(sheet).max_column_width(0)).to eq 2
   end
+
+  it 'formats all header cells to fit the longest value for a given column' do
+    SheetRender.new(sheet)
+  end
+
+  # it 'formats cells to match width of widest record/header value ' do
+  #   puts sheet
+  # end
 end
